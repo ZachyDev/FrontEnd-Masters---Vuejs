805cc21b60bfbac9dd2553febef625142f59e691
ZachyDev <mosetizachary001@gmail.com>
2020-09-04 12:32:57 +0300

    vue instance demo✅✅

---
+++ vuefromzero/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>VueFromZero</h1>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      msg: 'Master Vuejs'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
