87d639a39c89d24944dc63f66b3f3d62028bff1b
ZachyDev <mosetizachary001@gmail.com>
2020-09-04 12:39:17 +0300

    vue directives:v-model✅✅

--- vuefromzero/src/App.vue
+++ vuefromzero/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
     <Home />
+    <Form />
   </div>
 </template>
 
 <script>
 import Home from './components/Home';
+import Form from './components/Form';
 export default {
   components: {
-    Home
+    Home,
+    Form
   }
 }
 </script>
---
+++ vuefromzero/src/components/Form.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="form">
+      <form>
+          <input type="text" v-model="username">
+          {{ username }}
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Form",
+    data() {
+        return{
+            username: ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
