bb8e7666e7ceab8339213772ffb1125bc82b02bf
ZachyDev <mosetizachary001@gmail.com>
2020-09-04 12:50:03 +0300

    vue directives:v-for✅✅

--- vuefromzero/src/components/Form.vue
+++ vuefromzero/src/components/Form.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="form">
       <form>
+          <h2>Vue Directives</h2>
           <input type="text" v-model="username">
-          {{ username }}
+          {{ username }} <br />
+          <input type="checkbox" v-model="checkedNames" id="zachy">
+          <label for="zachy">Zachy</label>
+           <input type="checkbox" v-model="checkedNames" id="finna">
+          <label for="finna">Finna</label>
+           <input type="checkbox" v-model="checkedNames" id="willy"> 
+          <label for="willy">Willy</label><br />
+          <strong>CheckedNames</strong>{{ checkedNames }}
       </form>
   </div>
 </template>
@@ -12,12 +20,17 @@ export default {
     name: "Form",
     data() {
         return{
-            username: ''
+            username: '',
+            checkedNames: []
         }
     }
 }
 </script>
 
 <style scoped>
-
+    #form form{
+        margin:0 auto;
+        width: 50%;
+    }
+    
 </style>
